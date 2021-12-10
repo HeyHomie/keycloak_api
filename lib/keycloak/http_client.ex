de452fcaaaed7b2990d1c0299bb7f122630041e1
Plus17 <me@sergiovp.dev>
2021-12-10 12:38:14 -0600

    feat: adds http client behavior

--- lib/keycloak/http_client.ex
+++ lib/keycloak/http_client.ex
@@ -1,74 +1,33 @@
 defmodule KeycloakAPI.HTTPClient do
   @moduledoc """
-  HTTP Client
+  Specification for a KeycloakAPI HTTP client.
+  The client is configured as a `{module, initial_state}` tuple where the module
+  implements this behaviour and `initial_state` is returned by the `c:init/1`
+  callback.
+  The `c:init/1` callback gives an opportunity to perform some initialization tasks just once.
   """
+
+  @type method() :: atom()
+
+  @type url() :: binary()
+
+  @type status() :: non_neg_integer()
+
+  @type header() :: {binary(), binary()}
+
+  @type body() :: binary()
+
+  @type initial_state() :: map()
 
   @doc """
-  Creates a new user
+  Callback to make an HTTP request.
   """
-  @spec create_user(map()) :: {:ok, Tesla.Env.t()} | {:error, map()}
-  def create_user(user_params) do
-    with {:ok, %{"access_token" => access_token}} <- get_admin_access_token() do
-      client = build_client(access_token)
-      realm = get_env!(:realm)
-      Tesla.post(client, "/admin/realms/#{realm}/users", user_params)
-    end
-  end
+  @callback request(method(), url(), [header()], body(), opts :: keyword()) ::
+              {:ok, %{status: status, headers: [header()], body: body()}}
+              | {:error, Exception.t()}
 
-  @doc """
-  Get user info by the given id
-  """
-  @spec get_user(String.t()) :: {:ok, map()} | {:error, map()}
-  def get_user(user_id) do
-    with {:ok, %{"access_token" => access_token}} <- get_admin_access_token() do
-      client = build_client(access_token)
-      realm = get_env!(:realm)
-
-      Tesla.get(client, "/admin/realms/#{realm}/users/#{user_id}")
-    end
-  end
-
-  # Get admin access token
-  @spec get_admin_access_token() :: {:ok, map()} | {:error, map()}
-  defp get_admin_access_token() do
-    middleware = [
-      {Tesla.Middleware.BaseUrl, get_env!(:site)},
-      Tesla.Middleware.JSON,
-      Tesla.Middleware.FormUrlencoded
-    ]
-
-    client = Tesla.client(middleware)
-
-    params = %{
-      grant_type: "client_credentials",
-      client_id: get_env!(:client_id),
-      client_secret: get_env!(:client_secret)
-    }
-
-    case Tesla.post(client, "/realms/master/protocol/openid-connect/token", params) do
-      {:ok, %Tesla.Env{status: 200, body: body}} ->
-        {:ok, body}
-
-      {:ok, %Tesla.Env{status: _status, body: body}} ->
-        {:error, body}
-    end
-  end
-
-  # Builds a new client
-  @spec build_client(String.t()) :: {:ok, map()} | {:error, map()}
-  defp build_client(access_token) do
-    middleware = [
-      {Tesla.Middleware.BaseUrl, get_env!(:site)},
-      Tesla.Middleware.JSON,
-      {Tesla.Middleware.Headers, [{"Authorization", "Bearer #{access_token}"}]}
-    ]
-
-    Tesla.client(middleware)
-  end
-
-  # Gets an env variable
-  @spec get_env!(atom()) :: term()
-  defp get_env!(key) do
-    Application.fetch_env!(:keycloak_api, key)
+  @doc false
+  def request(module, method, url, headers, body, opts) do
+    module.request(method, url, headers, body, opts)
   end
 end
